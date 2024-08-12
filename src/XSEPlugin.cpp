622688f214205d0a752bb99e2b2feb1aa2f6ee41
AtomCrafty <atomcrafty@frucost.net>
2024-08-12 14:19:24 +0200

    clean up XSEPlugin.cpp

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -5,108 +5,35 @@
 #include "Integrations/Completionist.h"
 #include "Integrations/LOTD.h"
 #include "LootMenu.h"
-#include "LootMenuManager.h"
 #include "MenuVisibilityManager.h"
 #include "Papyrus/Papyrus.h"
 
-namespace
+void OnSKSEMessage(SKSE::MessagingInterface::Message* msg)
 {
-	class InputHandler :
-		public RE::BSTEventSink<RE::InputEvent*>
-	{
-	public:
-		static InputHandler* GetSingleton()
-		{
-			static InputHandler singleton;
-			return std::addressof(singleton);
-		}
+	switch (msg->type) {
+	case SKSE::MessagingInterface::kPostLoad:
 
-		static void Register()
-		{
-			auto input = RE::BSInputDeviceManager::GetSingleton();
-			input->AddEventSink(GetSingleton());
-			logger::info("Registered InputHandler"sv);
-		}
+		// This needs to run before kInputLoaded
+		Input::InputManager::Install();
 
-	protected:
-		using EventResult = RE::BSEventNotifyControl;
+		QuickLoot::API::APIServer::Init(SKSE::GetMessagingInterface());
+		break;
 
-		EventResult ProcessEvent(RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>*) override
-		{
-			using InputType = RE::INPUT_EVENT_TYPE;
-			using Keyboard = RE::BSWin32KeyboardDevice::Key;
+	case SKSE::MessagingInterface::kDataLoaded:
 
-			if (!a_event) {
-				return EventResult::kContinue;
-			}
+		QuickLoot::Papyrus::Init();
+		QuickLoot::LootMenu::Register();
+		QuickLoot::MenuVisibilityManager::InstallHooks();
 
-			auto intfcStr = RE::InterfaceStrings::GetSingleton();
-			auto ui = RE::UI::GetSingleton();
-			if (ui->IsMenuOpen(intfcStr->console)) {
-				return EventResult::kContinue;
-			}
+		QuickLoot::Behaviors::ActivationBlocker::Install();
 
-			for (auto event = *a_event; event; event = event->next) {
-				if (event->eventType != InputType::kButton) {
-					continue;
-				}
-
-				auto button = static_cast<RE::ButtonEvent*>(event);
-				if (!button->IsDown() || button->device != RE::INPUT_DEVICE::kKeyboard) {
-					continue;
-				}
-
-				switch (button->idCode) {
-				case Keyboard::kNum0:
-					QuickLoot::LootMenuManager::Enable();
-					break;
-				case Keyboard::kNum9:
-					QuickLoot::LootMenuManager::Disable();
-					break;
-				default:
-					break;
-				}
-			}
-
-			return EventResult::kContinue;
-		}
-
-	private:
-		InputHandler() = default;
-		InputHandler(const InputHandler&) = delete;
-		InputHandler(InputHandler&&) = delete;
-
-		~InputHandler() = default;
-
-		InputHandler& operator=(const InputHandler&) = delete;
-		InputHandler& operator=(InputHandler&&) = delete;
-	};
-
-	void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
-	{
-		switch (a_msg->type) {
-		case SKSE::MessagingInterface::kPostLoad:
-
-			// This needs to run before kInputLoaded
-			Input::InputManager::Install();
-			break;
-
-		case SKSE::MessagingInterface::kDataLoaded:
-
-			QuickLoot::Papyrus::Init();
-			QuickLoot::LootMenu::Register();
-			QuickLoot::MenuVisibilityManager::InstallHooks();
-
-			QuickLoot::Behaviors::ActivationBlocker::Install();
-
-			QuickLoot::Integrations::LOTD::Init();
-			QuickLoot::Integrations::Completionist::Init();
-			break;
-		}
+		QuickLoot::Integrations::LOTD::Init();
+		QuickLoot::Integrations::Completionist::Init();
+		break;
 	}
 }
 
-void InitializeLog([[maybe_unused]] spdlog::level::level_enum a_level = spdlog::level::info)
+void InitializeLog(spdlog::level::level_enum level = spdlog::level::info)
 {
 	auto path = logger::log_directory();
 	if (!path) {
@@ -115,30 +42,13 @@ void InitializeLog([[maybe_unused]] spdlog::level::level_enum a_level = spdlog::
 
 	*path /= std::format("{}.log"sv, Plugin::NAME);
 	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-
 	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-	log->set_level(a_level);
-	log->flush_on(a_level);
+
+	log->set_level(level);
+	log->flush_on(level);
 
 	spdlog::set_default_logger(std::move(log));
 	spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] [%t] [%s:%#] %v");
-}
-
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-{
-	InitializeLog();
-	logger::info("Loaded plugin {} {}", Plugin::NAME, Plugin::VERSION.string());
-	Init(a_skse);
-	SKSE::AllocTrampoline(1 << 6);
-
-	auto message = SKSE::GetMessagingInterface();
-	if (!message->RegisterListener(MessageHandler)) {
-		return false;
-	}
-
-	QuickLoot::API::APIServer::Init(message);
-
-	return true;
 }
 
 extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() noexcept {
@@ -153,7 +63,19 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() noexcept {
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* pluginInfo)
 {
 	pluginInfo->name = SKSEPlugin_Version.pluginName;
-	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
 	pluginInfo->version = SKSEPlugin_Version.pluginVersion;
+	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
 	return true;
+}
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse)
+{
+	InitializeLog();
+
+	logger::info("Loaded plugin {} {}", Plugin::NAME, Plugin::VERSION.string());
+
+	SKSE::Init(skse);
+	SKSE::AllocTrampoline(1 << 6);
+
+	return SKSE::GetMessagingInterface()->RegisterListener(OnSKSEMessage);
 }
