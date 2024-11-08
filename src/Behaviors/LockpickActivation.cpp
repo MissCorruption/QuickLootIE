87243e0ce6312bc6e62005924049857580aca24a
AtomCrafty <atomcrafty@frucost.net>
2024-11-08 19:31:31 +0100

    improve container menu suppression logic

--- src/Behaviors/LockpickActivation.cpp
+++ src/Behaviors/LockpickActivation.cpp
@@ -4,9 +4,22 @@ namespace QuickLoot::Behaviors
 {
 	struct ActivateRefHook
 	{
-		static void thunk(RE::TESObjectREFR* _this, RE::TESObjectREFR* activator, uint8_t a3, RE::TESObjectACTI* a4, int a5, char skipScriptEvent)
+		static void thunk1(RE::TESObjectREFR* _this, RE::TESObjectREFR* activator, uint8_t a3, RE::TESObjectACTI* a4, int a5, char skipScriptEvent)
 		{
-			if (LockpickActivation::IsBlocked()) {
+			thunk(_this, activator, a3, a4, a5, skipScriptEvent, func1);
+		}
+
+		static void thunk2(RE::TESObjectREFR* _this, RE::TESObjectREFR* activator, uint8_t a3, RE::TESObjectACTI* a4, int a5, char skipScriptEvent)
+		{
+			thunk(_this, activator, a3, a4, a5, skipScriptEvent, func2);
+		}
+
+		static inline REL::Relocation<decltype(thunk1)> func1;
+		static inline REL::Relocation<decltype(thunk2)> func2;
+
+		static void thunk(RE::TESObjectREFR* _this, RE::TESObjectREFR* activator, uint8_t a3, RE::TESObjectACTI* a4, int a5, char skipScriptEvent, const REL::Relocation<decltype(thunk1)>& func)
+		{
+			if (LockpickActivation::IsBlocked() && _this->GetObjectReference()->Is(RE::FormType::Container)) {
 				return;
 			}
 
@@ -17,17 +30,19 @@ namespace QuickLoot::Behaviors
 
 			func(_this, activator, a3, a4, a5, skipScriptEvent);
 		}
-
-		static inline REL::Relocation<decltype(thunk)> func;
 	};
 
 	void LockpickActivation::Install()
 	{
-		// We overwrite the call to TESObjectREFR::ActivateRef in LockpickingMenu::Unlock to prevent the container menu from opening.
+		// We overwrite the calls to TESObjectREFR::ActivateRef in LockpickingMenu::Unlock and TESObjectCONT::Activate to prevent the container menu from opening.
 		// Relocation IDs and offsets taken from https://github.com/Umgak/Skyrim-No-Lockpick-Activate/blob/master/src/main.cpp
 
-		REL::Relocation loc{ RELOCATION_ID(51088, 51968), REL::VariantOffset(0x1BE, 0x1C0, 0) };
-		ActivateRefHook::func = SKSE::GetTrampoline().write_call<5>(loc.address(), ActivateRefHook::thunk);
+		// SNLA uses ID(17922)+0x2E4 rather than ID(17887)+0x164, but that seems to be the wrong function.
+		REL::Relocation loc1{ RELOCATION_ID(17485, 17887), REL::VariantOffset(0x164, 0x164, 0) };
+		REL::Relocation loc2{ RELOCATION_ID(51088, 51968), REL::VariantOffset(0x1BE, 0x1C0, 0) };
+
+		ActivateRefHook::func1 = SKSE::GetTrampoline().write_call<5>(loc1.address(), ActivateRefHook::thunk1);
+		ActivateRefHook::func2 = SKSE::GetTrampoline().write_call<5>(loc2.address(), ActivateRefHook::thunk2);
 
 		logger::info("Installed {}", typeid(LockpickActivation).name());
 	}
