7949cd2622d417b01e6aa9ec1e0518709a0fbdb1
AtomCrafty <atomcrafty@frucost.net>
2024-11-08 05:34:52 +0100

    only clear text of UI messages if they are of type kActivate

--- src/Behaviors/ActivationPrompt.cpp
+++ src/Behaviors/ActivationPrompt.cpp
@@ -6,7 +6,7 @@ namespace QuickLoot::Behaviors
 	{
 		static void thunk(RE::UIMessageQueue* _this, const RE::BSFixedString& menuName, RE::UI_MESSAGE_TYPE type, RE::HUDData* data)
 		{
-			if (ActivationPrompt::IsBlocked()) {
+			if (ActivationPrompt::IsBlocked() && (data->type == RE::HUDData::Type::kActivate || data->type == RE::HUDData::Type::kActivateNoLabel)) {
 				data->text = "";
 				data->unk40 = false; // enable
 			}
@@ -19,6 +19,8 @@ namespace QuickLoot::Behaviors
 
 	void ActivationPrompt::Install()
 	{
+		// We intercept the call to UIMessageQueue::AddMessage in PlayerCharacter::UpdateCrosshairs.
+		// This is the same way Simple Activate and Which Key do it.
 		// Copied from https://github.com/powerof3/SimpleActivateSKSE/blob/master/src/Manager.cpp
 
 		REL::Relocation loc{ RELOCATION_ID(39535, 40621), REL::VariantOffset(0x289, 0x280, 0) };
