0fe2cd18cfd0e1439372e3b063131e02b438cca0
AtomCrafty <atomcrafty@frucost.net>
2024-12-27 11:04:16 +0100

    implement CrosshairRefObserver patch for VR

--- src/Observers/CrosshairRefObserver.cpp
+++ src/Observers/CrosshairRefObserver.cpp
@@ -6,20 +6,66 @@ namespace QuickLoot::Observers
 {
 	using enum RE::BSEventNotifyControl;
 
+	RE::ObjectRefHandle GetCurrentVRTarget()
+	{
+		const auto rightHanded = RE::PlayerCharacter::GetSingleton()->GetVRPlayerRuntimeData().isRightHandMainHand;
+		const auto hand = rightHanded ? RE::VR_DEVICE::kRightController : RE::VR_DEVICE::kLeftController;
+		const auto crosshair = RE::CrosshairPickData::GetSingleton();
+
+		auto currentTargetRef = crosshair->grabPickRef[hand];
+
+		if (!currentTargetRef) {
+			currentTargetRef = crosshair->targetActor[hand];
+		}
+
+		if (!currentTargetRef) {
+			currentTargetRef = crosshair->target[hand];
+		}
+
+		return currentTargetRef;
+	}
+
+	void CrosshairRefObserver::StartVRTargetPollThread()
+	{
+		std::thread t([] {
+			bool isReady = true;
+
+			while (true) {
+				if (isReady) {
+					isReady = false;
+
+					SKSE::GetTaskInterface()->AddTask([&] {
+						isReady = true;
+						MenuVisibilityManager::OnCrosshairRefChanged(GetCurrentVRTarget());
+					});
+				}
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(15));
+			}
+		});
+
+		t.detach();
+	}
+
 	void CrosshairRefObserver::Install()
 	{
-		SKSE::GetCrosshairRefEventSource()->AddEventSink(GetSingleton());
+		if (REL::Module::IsVR()) {
+			StartVRTargetPollThread();
+		} else {
+			SKSE::GetCrosshairRefEventSource()->AddEventSink(GetSingleton());
+		}
+
 		logger::info("Installed");
 	}
 
 	RE::BSEventNotifyControl CrosshairRefObserver::ProcessEvent(
-		const SKSE::CrosshairRefEvent* a_event,
+		const SKSE::CrosshairRefEvent* event,
 		RE::BSTEventSource<SKSE::CrosshairRefEvent>*)
 	{
 		auto handle = RE::ObjectRefHandle();
 
-		if (a_event && a_event->crosshairRef) {
-			handle = a_event->crosshairRef->CreateRefHandle();
+		if (event && event->crosshairRef) {
+			handle = event->crosshairRef->CreateRefHandle();
 		}
 
 		MenuVisibilityManager::OnCrosshairRefChanged(handle);
